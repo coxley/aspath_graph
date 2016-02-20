d4ec9dc4940a6629d96c223af4721e0d5b2aee98
Codey Oxley <codey@yelp.com>
2016-02-19 18:47:27 -0800

    Bump version to 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aspath_graph',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
