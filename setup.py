92d2302296b037188f516e43c1844bf83ce7949c
Codey Oxley <codey@yelp.com>
2016-02-20 03:50:01 -0800

    Tidying up packaging

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='aspath_graph',
-    version='1.1',
+    version='1.2',
+    description='Take BGP AS PATHs and generate an interactive javascript graph using NetJSON',
+    author='Codey Oxley',
+    author_email='codey.a.oxley+os@gmail.com',
+    url='https://github.com/coxley/aspath_graph',
+    download_url='https://github.com/coxley/aspath_graph/tarball/1.1',
+    keywords=['networking', 'bgp', 'asn', 'netjson', 'json', 'graph', 'd3', 'routes'],
+    classifiers=[],
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
